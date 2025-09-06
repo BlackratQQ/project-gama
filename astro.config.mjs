f46a99cf61d533e7bdd87965b5621c8af9c38606
BlackratQQ <vojtech.kochta@gmail.com>
2025-09-06 23:06:15 +0200

    Úprava .gitignore pro zahrnutí lokální složky Netlify a změna adaptéru v astro.config.mjs na @astrojs/netlify. Aktualizace závislostí v package.json a package-lock.json pro přidání @astrojs/netlify a odstranění @astrojs/node.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import icon from 'astro-icon';
-import node from '@astrojs/node';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: netlify(),
   integrations: [
     tailwind({
       applyBaseStyles: false
